9969c0abcaf3302e76cd78131eb6df2355d49fe5
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-08-14 12:37:29 -0300

    Delete RepositorioDoBem directory

--- RepositorioDoBem/src/main/java/com/br/gov/sp/fatec/RepositorioDoBem/Model/Repository/AlimentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.gov.sp.fatec.RepositorioDoBem.Model.Repository;
-
-import com.br.gov.sp.fatec.RepositorioDoBem.Model.Domain.Alimento;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AlimentoRepository extends MongoRepository<Alimento, String> {
-}
--- RepositorioDoBem/src/main/java/com/br/gov/sp/fatec/RepositorioDoBem/Model/Repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.gov.sp.fatec.RepositorioDoBem.Model.Repository;
-
-import com.br.gov.sp.fatec.RepositorioDoBem.Model.Domain.Usuario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-}
