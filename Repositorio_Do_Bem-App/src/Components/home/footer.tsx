6f85380881013c5137b8fdd13f3ba0efbe13a285
Mariana Julia <Thmessiasdev@gmail.com>
2024-09-03 10:33:48 -0300

    added lower block of home

---
+++ Repositorio_Do_Bem-App/src/Components/home/footer.tsx
@@ -0,0 +1,13 @@
+import './footer.css'
+
+export default function Footer(){
+    return (
+        <>
+        <footer className="footer-rb">
+        <div className="footer-logo">
+            <img src="./src/assets/images/logo-footer.png" alt='logo'/>
+        </div>
+        </footer>
+        </>
+    )
+}
