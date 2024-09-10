e965003cba3496418c31aebe2ee47e6c7fe5a0de
Thmsantos <Thiagomessias411@gmail.com>
2024-09-10 10:52:44 -0300

    added get and update aliments functions

---
+++ Repositorio_Do_Bem-App/src/Functions/stockFunction/getAllALimentos.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+function getAlimentos(){
+    axios.get("http://localhost:5555/getAllAlimentos")
+    .then((res) => {
+        console.log(res.data)
+    })
+}
+
+
+export default getAlimentos();
