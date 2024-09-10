e10d6b3898c5379355f60c7d9f48ad5ec0771140
Marigsiqueira <mgsmariana123@gmail.com>
2024-09-10 09:23:08 -0300

    added estilização of filtro and barra de pesquisa

--- Repositorio_Do_Bem-App/src/Pages/estoque.tsx
+++ Repositorio_Do_Bem-App/src/Pages/estoque.tsx
@@ -2,14 +2,17 @@ import Navebar from "../Components/estoque/navebar";
 import BarraDePesquisa from "../Components/estoque/barrapesquisar/barraDePesquisa";
 import StockTable from "../Components/estoque/stocktable/tabela";
 import Filtro from "../Components/estoque/filtro/dropdown";
+import '/src/Components/estoque/estoque.css'
 
 function Estoque(){
     return(
 
         <div>
             <Navebar />
-            <Filtro/>
-            <BarraDePesquisa/>
+            <div className="filter-search">
+                <Filtro/>
+                <BarraDePesquisa/>
+            </div>
             <StockTable />
         </div>
 
