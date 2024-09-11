1adc3c19722837af0165fdba0039dbe9f71a351b
Dandara Melo <dands20melo@gmail.com>
2024-09-11 09:35:17 -0300

    feat: Function addFood

---
+++ Repositorio_Do_Bem-App/src/Functions/stockFunction/addFood.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const adicionarAlimento = async () => {
+    const res = await axios.post("http://localhost:5555/saveAlimento", {
+        nome: "Ervilha", 
+        peso: "70g",
+        validade: "20/07/2026",
+        quantidade: "5",
+        emailDoador: "analaura@gmail.com" 
+    });
+    console.log(res.data);
+};
