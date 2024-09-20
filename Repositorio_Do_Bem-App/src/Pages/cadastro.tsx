1f3f65e433073f1cb5cdb0e605afd8d31b3efa8b
Dandara Melo <dands20melo@gmail.com>
2024-09-20 12:00:17 -0300

    Dandara { Cadastrar } -> Fixing wrong import and making header styles changes

--- Repositorio_Do_Bem-App/src/Pages/cadastro.tsx
+++ Repositorio_Do_Bem-App/src/Pages/cadastro.tsx
@@ -1,12 +1,12 @@
 import Cadastro_Form from "../Components/cadastro/cadastro-form";
-import Header from "../Components/login/header";
 import Left_panel from "../Components/cadastro/left-panel";
 import '../Components/cadastro/cadastro-form.css';
+import HeaderSignUp from "../Components/cadastro/header";
 
 function Cadastro() {
     return (
         <>
-            <Header />
+            <HeaderSignUp />
             <div id="conteudo">
                 <div><Left_panel /></div>
                 <div><Cadastro_Form /></div>
