20bae9ab8db2146dcf8a6d53a820bd50b0639094
Dandara Melo <dands20melo@gmail.com>
2024-09-20 12:01:27 -0300

    Dandara { Cadastrar } -> Making styles changes in signup

--- Repositorio_Do_Bem-App/src/Components/cadastro/cadastro-form.tsx
+++ Repositorio_Do_Bem-App/src/Components/cadastro/cadastro-form.tsx
@@ -49,6 +49,10 @@ function Cadastro_Form() {
             <div id="direita">
                 <h2>Cadastre-se</h2>
                     <div id="inputinhos">
+                    <div className="input-box">
+                        <img src="./src/assets/images/NOME.png" alt="nome" />
+                        <input name="nome" type="text" placeholder="Nome" onChange={(e) => setNome(e.target.value)} />
+                    </div>
                         <div className="input-box">
                             <img src="./src/assets/images/EMAIL.png" alt="email" />
                             <input name="email" type="text" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} />
@@ -61,23 +65,19 @@ function Cadastro_Form() {
                             <img src="./src/assets/images/SENHA.png" alt="senha" />
                             <input name="senha" type="password" placeholder="Senha" onChange={(e) => setSenha(e.target.value)} />
                         </div>
-                    </div>
-                    <div id="linha">
-                        <div className="input-linha">
-                            <img src="./src/assets/images/CEP.png" alt="CEP" />
-                            <input name="cep" type="text" placeholder="CEP"  onChange={(e) => setCep(e.target.value)} />
+                        <div id="linha">
+                            <div className="input-linha" id="cep">
+                                <img src="./src/assets/images/CEP.png" alt="CEP" />
+                                <input name="cep" type="text" placeholder="CEP"  onChange={(e) => setCep(e.target.value)} />
+                            </div>
+                            <div className="input-linha" id="num">
+                                <input name="numero" type="text" placeholder="Nº"  onChange={(e) => setNumero(e.target.value)}/>
+                            </div>
                         </div>
-                        <div className="input-linha">
-                            <img src="./src/assets/images/CEP.png" alt="numero" />
-                            <input name="numero" type="text" placeholder="Número"  onChange={(e) => setNumero(e.target.value)}/>
-                        </div>
                     </div>
-                    <div className="input-box" id="nome">
-                        <img src="./src/assets/images/NOME.png" alt="nome" />
-                        <input name="nome" type="text" placeholder="Nome" onChange={(e) => setNome(e.target.value)} />
-                    </div>
+                    
                     <div id="btn">
-                        <button onClick={registerUser}>Entrar</button>
+                        <button onClick={registerUser}>Cadastrar</button>
                         <a href="../login">Já possui uma conta?</a>
                     </div>
             </div>
