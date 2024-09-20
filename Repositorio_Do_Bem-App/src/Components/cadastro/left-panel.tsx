e72111ab9ec47fc29dc5155b68f36a5e8b046d26
Dandara Melo <dands20melo@gmail.com>
2024-09-20 12:02:59 -0300

    Dandara { Cadastrar } -> Making changes to text style

--- Repositorio_Do_Bem-App/src/Components/cadastro/left-panel.tsx
+++ Repositorio_Do_Bem-App/src/Components/cadastro/left-panel.tsx
@@ -2,7 +2,7 @@ import "./left-panel.css"
 function Left_panel (){
     return(
         <div className="left-panel">
-            <h1>Welcome <br /> Back!</h1>
+            <h1>Bem-vindo</h1>
             <p>Pronto para otimizar sua gestão de doações de alimentos com o melhor sistema disponível?</p>
         </div>
     )
