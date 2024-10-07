e3db8d5ecab2f600f972e806983ea38df276340d
KaueS771 <kaue.henrique.santos2005@gmail.com>
2024-10-07 11:16:37 -0300

    Thiago { CRUD ESTOQUE } -> att function Get Aliments in table

--- Repositorio_Do_Bem-App/src/Components/estoque/stocktable/tabela.tsx
+++ Repositorio_Do_Bem-App/src/Components/estoque/stocktable/tabela.tsx
@@ -2,19 +2,39 @@ import './tabela.css';
 import GetAlimentos from '../../../Functions/stockFunction/GetAlimentos';
 import { AlimentoData } from '../../../Functions/stockFunction/Interfaces/AlimentoData';
 import { useEffect, useState } from 'react';
+import ModalProps2 from '../../modal_props2/modalProps';
 
-function StockTable (){
+function StockTable() {
     const [alimentos, setAlimentos] = useState<AlimentoData[]>([]);
+    const [isEditOpen, setEditOpen] = useState(false);
+    const [alimentoEdit, setAlimentoEdit] = useState<AlimentoData>();
+    const [nome, setNome] = useState<string>('');
+    const [peso, setPeso] = useState<string>('');
+    const [data, setData] = useState<string>('');
 
     useEffect(() => {
         const fetchAlimentos = async () => {
             const alimentosAxios = await GetAlimentos();
-            console.log(alimentos)
-            setAlimentos(alimentosAxios); 
+            setAlimentos(alimentosAxios);
         };
 
         fetchAlimentos();
     }, []);
+
+    function EditChange(alimentoId: string){
+        for(let i = 0; i < alimentos.length; i++){
+            if(alimentos[i].alimentoId === alimentoId){
+                setAlimentoEdit(alimentos[i])
+            }
+        }
+
+        setEditOpen(true);
+    }
+
+    function EditAlimento(){
+        //construir o objeto 
+    }
+
 
     return (
         <>
@@ -23,23 +43,47 @@ function StockTable (){
                     <thead>
                         <th>Alimento</th>
                         <th>Peso</th>
-                        <th>Flag</th>
                         <th>Validade</th>
                     </thead>
                     <tbody>
                         {alimentos.map((alimento) => (
-                            <tr>
+                            <tr key={alimento.alimentoId}>
                                 <td>{alimento.nome}</td>
                                 <td>{alimento.peso}</td>
-                                <td>{alimento.flag}</td>
                                 <td>{alimento.validade}</td>
+                                <td>
+                                    <button onClick={() => EditChange(alimento.alimentoId)}>Editar</button>
+                                </td>
+                                <td>
+                                    <button>Excluir</button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
+
+            <ModalProps2 isOpen={isEditOpen} setOpen={setEditOpen} title="Editar Alimento">
+                <input
+                    placeholder="Nome"
+                    defaultValue={alimentoEdit?.nome}
+                    onChange={(e) => setNome(e.target.value)}
+                />
+                <input
+                    placeholder="Peso"
+                    defaultValue={alimentoEdit?.peso}
+                    onChange={(e) => setPeso(e.target.value)}
+                />
+                <input
+                    placeholder="Validade"
+                    defaultValue={alimentoEdit?.validade}
+                    type="date"
+                    onChange={(e) => setData(e.target.value.toString())}
+                />
+                <button>Salvar</button>
+            </ModalProps2>
         </>
-        
+
     )
 }
 
