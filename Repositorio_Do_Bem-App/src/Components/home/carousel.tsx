c73daed4ff03f2a3e072b22249fc703b0061078d
Yasminoliveirat <yasminoliveira_santos@hotmail.com>
2024-10-08 21:23:38 -0300

    Yasmin { ESTILIZAÇÃO } -> header/carousel

--- Repositorio_Do_Bem-App/src/Components/home/carousel.tsx
+++ Repositorio_Do_Bem-App/src/Components/home/carousel.tsx
@@ -1,20 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import './carousel.css';
 
+const images = [
+    './src/assets/images/imagem1.png',
+    './src/assets/images/imagem02.jpg',
+    './src/assets/images/imagem03.jpg',
+];
+
 export default function Carousel() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextImage = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const prevImage = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    };
+
+    // Adicionando autoplay com useEffect
+    useEffect(() => {
+        const interval = setInterval(() => {
+            nextImage(); // Troca de imagem a cada intervalo
+        }, 3000); // 3 segundos de intervalo (ajuste conforme necessário)
+
+        return () => {
+            clearInterval(interval); // Limpa o intervalo ao desmontar o componente ou mudar o índice
+        };
+    }, [currentIndex]);
+
     return (
-        <>
-            <div className="carousel">
-                <div className="carousel-container">
-                    <div className="carousel-item">
-                        <img src="./src/assets/images/imagem1.png" alt="Imagem 1" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="./src/assets/images/imagem1.png" alt="Imagem 2" />
-                    </div>
+        <div className="carousel">
+            <div className="carousel-container">
+                <div className="carousel-item">
+                    <img src={images[currentIndex]} alt={`Imagem ${currentIndex + 1}`} />
                 </div>
-                <button className="carousel-prev">&#10094;</button>
-                <button className="carousel-next">&#10095;</button>
             </div>
-        </>
+            <button className="carousel-prev" onClick={prevImage}>&#10094;</button>
+            <button className="carousel-next" onClick={nextImage}>&#10095;</button>
+        </div>
     );
 }
