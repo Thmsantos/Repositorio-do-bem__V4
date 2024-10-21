87e01f584fc66cc01320fc0494f1915b7cc997ab
Dandara Melo <dands20melo@gmail.com>
2024-10-21 02:12:59 -0300

    Dandara { CRUD ESTOQUE } -> função de debitar alimento

--- Repositorio_Do_Bem-App/src/Components/estoque/Botoes/botao.tsx
+++ Repositorio_Do_Bem-App/src/Components/estoque/Botoes/botao.tsx
@@ -1,24 +1,37 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import './botao.css';
 import adicionarAlimento from '../../../Functions/stockFunction/adicionarAlimento';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import generatePdf from '../../../Functions/stockFunction/generatePDF';
 import ModalProps2 from '../../modal_props2/modalProps';
+import { debitarAlimento } from '../../../Functions/stockFunction/debitarAlimentos';
+import { AlimentoData } from '../../../Functions/stockFunction/Interfaces/AlimentoData';
+import GetAlimentos from '../../../Functions/stockFunction/GetAlimentos';
 
 function Botao() {
   const [peso, setPeso] = useState<string>('')
   const [validade, setValidade] = useState<string>('')
   const [nome, setNome] = useState<string>('')
+  const [alimentos, setAlimentos] = useState<AlimentoData[]>([]); 
+  
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isDoacaoModalOpen, setIsDoacaoModalOpen] = useState(false);
+  const [selectedAlimento, setSelectedAlimento] = useState<string>(''); 
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openAddModal = () => setIsAddModalOpen(true);
+  const closeAddModal = () => setIsAddModalOpen(false);
 
-  const openModal = () => {
-    setIsModalOpen(true)
-  }
+  const openDoacaoModal = () => setIsDoacaoModalOpen(true);
+  const closeDoacaoModal = () => setIsDoacaoModalOpen(false);
 
-  const closeModal = () => {
-    setIsModalOpen(false)
-  }
+  useEffect(() => {
+    const fetchAlimentos = async () => {
+        const alimentosAxios = await GetAlimentos();
+        setAlimentos(alimentosAxios);
+    };
+
+    fetchAlimentos();
+}, []);
 
   function handleAddAlimento() {
     const changeAlimento = {
@@ -33,7 +46,16 @@ function Botao() {
   function addAlimento() {
     const newAlimento = handleAddAlimento();
     adicionarAlimento(newAlimento); 
-    closeModal();
+    closeAddModal();
+  }
+
+  async function handleFazerDoacao() {
+    if (selectedAlimento) {
+      await debitarAlimento(selectedAlimento, setAlimentos);
+      closeDoacaoModal(); 
+    } else {
+      alert('Selecione um alimento para doar.');
+    }
   }
 
   return (
@@ -41,12 +63,12 @@ function Botao() {
       <div className="button-container">
         <div> <button className="generate-report" onClick={generatePdf}>Gerar Relatório</button></div>
         <div className="button-group">
-          <button className="botoes" onClick={() => {openModal()}}>+ Add alimento</button>
-          <button className="botoes">- Fazer doação</button>
+          <button className="botoes" onClick={openAddModal}>+ Add alimento</button>
+          <button className="botoes" onClick={openDoacaoModal}>- Fazer doação</button>
         </div>
       </div>
 
-      <ModalProps2 isOpen={isModalOpen} setOpen={setIsModalOpen} title="Adicionar Novo Alimento">
+      <ModalProps2 isOpen={isAddModalOpen} setOpen={setIsAddModalOpen} title="Adicionar Novo Alimento">
         <input
           placeholder="Nome do Alimento"
           onChange={(e) => setNome(e.target.value)}
@@ -65,10 +87,23 @@ function Botao() {
         />
         <br></br>
         <button onClick={() => {addAlimento(), window.location.reload()}}>Adicionar</button>
-        <button onClick={closeModal}>Cancelar</button>
+        <button onClick={closeAddModal}>Cancelar</button>
       </ModalProps2>
 
 
+      <ModalProps2 isOpen={isDoacaoModalOpen} setOpen={setIsDoacaoModalOpen} title="Fazer Doação">
+        <select value={selectedAlimento} onChange={(e) => setSelectedAlimento(e.target.value)}>
+          <option value="">Selecione os alimentos para doar</option>
+          {alimentos.map((alimento) => (
+            <option key={alimento.alimentoId} value={alimento.alimentoId}>
+              {alimento.nome} - {alimento.peso}
+            </option>
+          ))}
+        </select>
+        <br />
+        <button onClick={handleFazerDoacao}>Confirmar Doação</button>
+        <button onClick={closeDoacaoModal}>Cancelar</button>
+      </ModalProps2>
     </>
   );
 }
